8cec84180b4716b2962ea2d85912f0af4898a065
sandipjha762 <sandipjha762@gmail.com>
2021-04-28 15:05:19 +0530

    added css

---
+++ src/Hey.js
@@ -0,0 +1,13 @@
+
+import './Hey.css';
+import React from 'react';
+class Hey extends React.Component{
+    render(){
+        return (
+            <div className="Box">Hello</div>
+        )
+            
+        
+    }
+}
+export default Hey;
